539c4a08738708c194a173c7784112f146886aea
shudhanshushekhar123 <shudhanshuraz01@gmail.com>
2024-03-05 13:45:49 +0530

    add

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cors = require("cors")
+
+
+const app = express();
+
+const PORT = 5000;
+app.use(cors({
+    origin: "https://65e6cfd406fa0969846450d2--glistening-alpaca-63366f.netlify.app",
+    credentials: true
+}));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60000,
+      
+    }
+}));
+
+
+
+
+
+app.post('/register', (req, res) => {
+   
+    const { username, password } = req.body;
+
+    req.session.user = username;
+
+    res.status(201).send({ msg: req.session.user })
+
+});
+
+
+app.get('/session', (req, res) => {
+     console.log(req.session)
+    res.send(  req.session.user )
+
+});
+
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            res.status(500).send('Error logging out');
+        } else {
+            res.send('Logout successful');
+        }
+    });
+});
+
+
+app.get('/checkLogin', (req, res) => {
+    console.log(req.session)
+    if (req.session.user) {
+        res.send(req.session.user);
+    } else {
+        res.status(401).send('Not logged in');
+    }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
